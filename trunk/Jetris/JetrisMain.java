023488759a334b30fe11ebd8848fa145a6b002a0
integrate.this <integrate.this@d5325c67-2a3d-0410-b24b-054266e8358d>
2007-10-29 22:20:10 +0000

    Added missing main class

---
+++ trunk/Jetris/JetrisMain.java
@@ -0,0 +1,9 @@
+import net.sourceforge.jetris.PlayerOption;
+
+public class JetrisMain {
+    public static void main(String[] args) {
+        PlayerOption playerOption = new PlayerOption();
+        
+    }
+
+}
