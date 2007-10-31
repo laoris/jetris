fb8a86a0113e91da7f8740f9259cc4469e22b102
jerkelens@bellsouth.net <jerkelens@bellsouth.net@d5325c67-2a3d-0410-b24b-054266e8358d>
2007-10-31 01:32:55 +0000

    Changed the main method to display new Player Selection GUI.

--- Jetris/JetrisMain.java
+++ Jetris/JetrisMain.java
@@ -1,8 +1,13 @@
-import net.sourceforge.jetris.PlayerOption;
+import net.sourceforge.jetris.NewGame;
+import javax.swing.*;
 
 public class JetrisMain {
     public static void main(String[] args) {
-        PlayerOption playerOption = new PlayerOption();
+		NewGame game = new NewGame();
+		game.setSize(400, 245);  //was(300,200)
+		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.setVisible(true);
+		game.setResizable(false);
         
     }
 
